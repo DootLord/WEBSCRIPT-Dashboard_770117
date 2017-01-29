0762c1cceb7f41adccbf90761c7993f10a665bb9
Shaun Porter <shaunoman840@gmail.com>
2017-01-29 16:11:39 +0000

    Re-styling of dashboard.
    Going for a smooth dark theme. With white text and flat red,blue and
    green highlights

--- webpage/js/jqueryscript.js
+++ webpage/js/jqueryscript.js
@@ -1,3 +1,25 @@
-$(function() {
-    $(".draggable").draggable();
+$(document).ready(function(){
+  dragFunc();
 });
+
+
+/*
+  Allows elements to become draggable by clicking a button
+*/
+
+function dragFunc(){
+  var isDraggable = false;
+  $(".draggable").draggable();
+  $(".draggable").draggable("disable");
+
+  $("#dragButton").click(function(){
+    if(isDraggable){
+      $(".draggable").draggable("disable");
+      isDraggable = false;
+    }
+    else{
+      $(".draggable").draggable("enable");
+      isDraggable = true;
+    }
+  });
+}
