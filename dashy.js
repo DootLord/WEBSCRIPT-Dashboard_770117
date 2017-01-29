29bbe6adcbd3910de7f477ca840d706c62d44b34
Shaun Porter <shaunoman840@gmail.com>
2017-01-29 18:24:12 +0000

    Updated styling and added spacing.
    Looks really nice now. Wanna implement a todo-list next! :)
    :calendar:

--- dashy.js
+++ dashy.js
@@ -4,9 +4,10 @@ var func = require("./js/func")
 
 app.get("/time", function (req,res){
   res.send(func.getTime());
-  console.log("Got get")
 });
 
 app.use(express.static(__dirname + "/webpage"));
 
-app.listen(8080);
+app.listen(8080, function(){
+  console.log("Started on port 8080");
+});
